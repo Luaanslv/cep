7788954ae5ac33b432f43d0bab0144515292bda1
Luaanslv <luaan.silva117@gmail.com>
2022-01-28 15:20:07 -0300

    Buscador de cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import'./style.css';
+import './style.css';
+import { useState } from 'react';
+import api from './services/api';
 function App() {
+  const [input, setInput] = useState('');
+  const [ cep, setCep ] = useState({});
+
+  async function handleSearch() {
+      //01310930 /json/
+    if (input === '') {
+      alert("preencha algum cep!")
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("erro ao buscar aqui!")
+      setInput("");
+    }
+    }
+
   return (
     <div className="container">
      <h1 className="title">Buscador de Cep</h1>
       <div className="container-input">
-        <input type="text" placeholder="Digite seu cep..." />
-        <button className="buttonSearch">
-      <i class="fas fa-search"></i>
-      </button>
+        <input type="text" placeholder="Digite seu cep..." value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button className="buttonSearch" onClick={handleSearch}>
+          <i class="fas fa-search"></i>
+        </button>
       </div>
       
-      <main>
-        <h2>Cep:541224112</h2>   
+
+      {Object.keys(cep).length > 0 && (
+        <main>
+          <h2>Cep: {cep.cep}</h2>
         
-        <span>Rua seila da onde</span>
-        <span>complemento:algum</span>
-        <span>Vila creuza</span>
-        <span>Itapevi-SP</span>
-      </main>
+          <span>{cep.logradouro }</span>
+          <span>complemento:{cep.complemento }</span>
+          <span>{cep.bairro }</span>
+          <span>{cep.localidade}-{ cep.uf}</span>
+        </main>
+      )}
+     
     </div>
     
   );
